530ae10454adedefe8c1205c61c76ec6f6f02307
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-04-26 01:42:38 +0900

    Added list

--- app/src/main/java/ch/skew/remotrix/MainActivity.kt
+++ app/src/main/java/ch/skew/remotrix/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -15,11 +21,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import ch.skew.remotrix.ui.theme.RemotrixTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainContent()
+            MainScreen()
         }
     }
 }
@@ -27,7 +34,7 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showSystemUi = true)
 @Composable
-fun MainContent() {
+fun MainScreen() {
     RemotrixTheme {
         Scaffold(
             topBar = {
@@ -37,9 +44,27 @@ fun MainContent() {
             }
         ) { padding ->
             Box(Modifier.padding(padding)){
-                Text("12")
+                MainContent()
             }
         }
     }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainContent(){
+    Column {
+        ListItem(
+            headlineText = { Text(stringResource(R.string.manage_accounts)) },
+            supportingText = { Text(stringResource(R.string.manage_accounts_desc)) },
+            leadingContent = {
+                Icon(
+                    Icons.Filled.AccountCircle,
+                    contentDescription = stringResource(R.string.manage_accounts),
+                )
+            }
+        )
+        Divider()
+
+    }
 }
