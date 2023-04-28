e55ed884582f0938945b2cee711c2582fb730bd7
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-04-28 22:27:29 +0900

    Form with labels and paddings

--- app/src/main/java/ch/skew/remotrix/ui/theme/NewAccount.kt
+++ app/src/main/java/ch/skew/remotrix/ui/theme/NewAccount.kt
@@ -1,6 +1,8 @@
 package ch.skew.remotrix.ui.theme
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -16,13 +18,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ch.skew.remotrix.R
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun NewAccount(
     onClickGoBack: () -> Unit = {}
 ){
+    val formPadding = Modifier
+        .fillMaxWidth()
+        .padding(
+            top = 10.dp,
+            start = 10.dp,
+            end = 10.dp
+        )
     Scaffold(
         topBar = {
             TopAppBar(
@@ -35,22 +47,34 @@ fun NewAccount(
             )
         }
     ) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
             val username = remember{ mutableStateOf("") }
             val password = remember{ mutableStateOf("") }
             val baseUrl = remember{ mutableStateOf("") }
             TextField(
+                modifier = formPadding,
                 value = username.value,
-                onValueChange = { username.value = it }
+                onValueChange = { username.value = it },
+                label = { Text(stringResource(R.string.username)) },
+                singleLine = true
             )
             TextField(
+                modifier = formPadding,
                 value = password.value,
-                onValueChange = { password.value = it }
+                onValueChange = { password.value = it },
+                label = { Text(stringResource(R.string.password)) },
+                singleLine = true
             )
             TextField(
+                modifier = formPadding,
                 value = baseUrl.value,
                 onValueChange = { baseUrl.value = it },
-                label = { Text(stringResource(R.string.homeserver_url_label)) }
+                label = { Text(stringResource(R.string.homeserver_url_label)) },
+                singleLine = true
             )
         }
     }
