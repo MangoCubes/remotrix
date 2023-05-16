0c166a496969179c6e9a54de4f33f211832a51a8
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-05-17 00:00:06 +0900

    Entered username and space ID are saved into settings

--- app/src/main/java/ch/skew/remotrix/Setup.kt
+++ app/src/main/java/ch/skew/remotrix/Setup.kt
@@ -1,8 +1,11 @@
 package ch.skew.remotrix
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
@@ -12,12 +15,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ch.skew.remotrix.data.RemotrixSettings
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
@@ -34,6 +40,7 @@ fun SetupScreen() {
     val settings = RemotrixSettings(context)
     val currentId = remember { mutableStateOf("") }
     val currentSpace = remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             TopAppBar({
@@ -78,6 +85,28 @@ fun SetupScreen() {
                 placeholder = { Text(stringResource(R.string.sample_space_id)) },
                 modifier = formPadding
             )
+            Button(
+                modifier = formPadding,
+                content = { Text(stringResource(R.string.confirm)) },
+                onClick = { onConfirm(currentId.value, currentSpace.value, context, scope, settings) }
+            )
         }
+    }
+}
+
+fun onConfirm(userId: String, spaceId: String, context: Context, scope: CoroutineScope, settings: RemotrixSettings){
+    val idPattern = Regex("@[A-z0-9]+:[A-z0-9\\.]+")
+    val spacePattern = Regex("![A-z]+:[A-z0-9\\.]+")
+    if(idPattern.matchEntire(userId) === null){
+        Toast.makeText(context, context.getString(R.string.invalid_id), Toast.LENGTH_SHORT).show()
+        return
+    }
+    if(spacePattern.matchEntire(spaceId) === null){
+        Toast.makeText(context, context.getString(R.string.invalid_space_id), Toast.LENGTH_SHORT).show()
+        return
+    }
+    scope.launch {
+        settings.saveId(userId)
+        settings.saveSpaceId(spaceId)
     }
 }
--- app/src/main/java/ch/skew/remotrix/data/RemotrixSettings.kt
+++ app/src/main/java/ch/skew/remotrix/data/RemotrixSettings.kt
@@ -13,16 +13,27 @@ class RemotrixSettings(
     companion object {
         private val Context.dataStore by preferencesDataStore(name = "settings")
         val managerId = stringPreferencesKey("managerId")
+        val spaceId = stringPreferencesKey("spaceId")
     }
 
     val getId: Flow<String> = context.dataStore.data.map { preferences ->
         preferences[managerId] ?: ""
     }
 
+    val getSpaceId: Flow<String> = context.dataStore.data.map { preferences ->
+        preferences[spaceId] ?: ""
+    }
+
 
     suspend fun saveId(name: String) {
         context.dataStore.edit { preferences ->
             preferences[managerId] = name
+        }
+    }
+
+    suspend fun saveSpaceId(id: String) {
+        context.dataStore.edit { preferences ->
+            preferences[spaceId] = id
         }
     }
 }
