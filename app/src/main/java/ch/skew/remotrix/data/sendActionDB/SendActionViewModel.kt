8796ced36b7c6055c707cef691ffb1ffd41876bb
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-06-09 22:02:42 +0900

    Removing unused functions

--- app/src/main/java/ch/skew/remotrix/data/sendActionDB/SendActionViewModel.kt
+++ app/src/main/java/ch/skew/remotrix/data/sendActionDB/SendActionViewModel.kt
@@ -4,21 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 
 class SendActionViewModel(
     private val dao: SendActionDao
 ): ViewModel() {
     val sendActions = dao.getAllAsFlow().stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
-
-    fun onEvent(event: SendActionEvent) {
-        when(event) {
-            is SendActionEvent.AddSendEvent -> viewModelScope.launch {
-                dao.insert(event.sendAction)
-            }
-            is SendActionEvent.DeleteSendEvent -> viewModelScope.launch {
-                dao.delete(event.sendAction)
-            }
-        }
-    }
 }
