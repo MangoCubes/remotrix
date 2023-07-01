869f32d1e2c2e02be74d51fa0d871ff6ae95a4a3
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-07-01 22:54:47 +0900

    Service watcher has been added to ensure service is running

--- app/src/main/java/ch/skew/remotrix/receiver/OnBoot.kt
+++ app/src/main/java/ch/skew/remotrix/receiver/OnBoot.kt
@@ -3,17 +3,31 @@ package ch.skew.remotrix.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import ch.skew.remotrix.background.CommandService
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import ch.skew.remotrix.background.ServiceWatcher
+import java.time.Duration
 
 class OnBoot : BroadcastReceiver(){
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onReceive(context: Context?, intent: Intent?) {
         if(intent?.action.equals(Intent.ACTION_BOOT_COMPLETED)){
             if(context == null) return
-            Intent(context, CommandService::class.java)
-                .apply {
-                    action = CommandService.START_ALL
-                    context.startService(this)
-                }
+            val work = PeriodicWorkRequestBuilder<ServiceWatcher>(Duration.ofHours(1))
+                .setConstraints(
+                    Constraints.Builder()
+                        .setRequiredNetworkType(
+                            NetworkType.CONNECTED
+                        ).build()
+                )
+                .build()
+            val workManager = WorkManager.getInstance(context)
+            workManager.enqueueUniquePeriodicWork("service_watcher", ExistingPeriodicWorkPolicy.KEEP, work)
         }
     }
 }
