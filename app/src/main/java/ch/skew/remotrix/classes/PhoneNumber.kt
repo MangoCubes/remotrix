a98a432b07085b8d71c5124026af036be6be4850
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2023-07-07 14:55:06 +0900

    Phone number class

---
+++ app/src/main/java/ch/skew/remotrix/classes/PhoneNumber.kt
@@ -0,0 +1,24 @@
+package ch.skew.remotrix.classes
+
+class InvalidPhoneNumber: Throwable()
+
+class PhoneNumber(private val number: String) {
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PhoneNumber) return false
+        return other.number == this.number
+    }
+
+    companion object {
+        fun from(number: String): Result<PhoneNumber>{
+            val filtered = number.filter { it.isDigit() }
+            return if(filtered.isEmpty()) Result.failure(InvalidPhoneNumber())
+            else Result.success(PhoneNumber(filtered))
+        }
+    }
+}
